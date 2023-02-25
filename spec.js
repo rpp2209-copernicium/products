3b4ff9ebfcf6f6ba631e864f0db3807df3addb03
Tiger Hong <tigerhong186@gmail.com>
2023-02-24 17:42:58 -0800

    Created gitignore file for node_modules. Created test suite and file.

--- spec.js
+++ spec.js
@@ -1,1 +1,11 @@
-// main testing file
+// test suite test
+
+
+describe('initial test suite check', () => {
+  test('should add up', () => {
+    var sum = function (num) {
+      return 1 + num;
+    }
+    expect(sum(3)).toBe(4)
+  })
+})
